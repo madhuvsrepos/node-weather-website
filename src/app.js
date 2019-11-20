697f17e5772dec2f23e1a40eb19b95e918b64ee9
madhu_vedhagirisubra <madhu_vedhagirisubra@dell.com>
2019-11-20 14:03:32 -0600

    updates

--- src/app.js
+++ src/app.js
@@ -5,45 +5,50 @@ const hbs = require('hbs')
 const app = express()
 const port = process.env.PORT || 3000
 //define paths for Express config
-const publicDirectoryPath = path.join(__dirname,'../public')
+const publicDirectoryPath = path.join(__dirname, '../public')
 //views is the default folder hbs looks for if you want to use a a different folder do then set it explicitly
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
 
 //setup handledbars engine and views location
 app.set('view engine', 'hbs')
-app.set('views',viewsPath);
+app.set('views', viewsPath);
 hbs.registerPartials(partialsPath)
 
 //setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
-app.get('',(req,res) => {
-     res.render('index', {title: 'Weather', name:'MVS'})
- })
-app.get('/about',(req,res) => {
-    res.render('about',{title: 'About me', name: 'MVS'})
+app.get('', (req, res) => {
+    res.render('index', { title: 'Weather', name: 'MVS' })
 })
-app.get('/help',(req,res) => {
-    res.render('help',{title: 'Help page', name: 'MVS'})
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About me', name: 'MVS' })
 })
-app.get('/random',(req,res) => {
-    res.send({info: "random render" })
+app.get('/help', (req, res) => {
+    res.render('help', { title: 'Help page', name: 'MVS' })
+})
+app.get('/random', (req, res) => {
+    res.send({ info: "random render" })
 })
 app.get('/weather', (req, res) => {
-    console.log(req.query.city);
-    const weatherData = geocode.geocodeFinder(req.query.city, geocode.weatherFinder, (weatherData) => {
-        console.log('weatherData', weatherData)
-        res.send(weatherData)
-    })
+    try {
+        console.log(req.query.city);
+        const weatherData = geocode.geocodeFinder(req.query.city, geocode.weatherFinder, (weatherData) => {
+            console.log('weatherData', weatherData)
+            res.send(weatherData)
+        })
+    }
+    catch (error) {
+        console.error(error)
+    }
 })
 
-app.get('/help/*',(req,res) => {
-    res.render('help404Page',{pageHeader:'Help Header', pageFooter:'Help Footer'})
+app.get('/help/*', (req, res) => {
+    res.render('help404Page', { pageHeader: 'Help Header', pageFooter: 'Help Footer' })
 })
-app.get('*',(req,res) => {
-    res.render('404Page',{pageHeader:'Header', pageFooter:'Footer'})
+app.get('*', (req, res) => {
+    res.render('404Page', { pageHeader: 'Header', pageFooter: 'Footer' })
 })
 app.listen(port, () => {
-    console.log('server started on port '+port)
+    console.log('server started on port ' + port)
 })
