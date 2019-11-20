23c79d53a83547aca1ecc47b54367de8fefc07b9
madhu_vedhagirisubra <madhu_vedhagirisubra@dell.com>
2019-11-20 14:10:15 -0600

    updates

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,12 @@
 console.log('client side javascript is loaded')
 const url = "/weather?city=round%20rock";
-fetch(url).then((response)=> {
-    response.json().then((data)=> {
-        if(data.error){
+fetch(url).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
             console.log(data.error)
         }
-        else{
-        console.log(data);
+        else {
+            console.log(data);
         }
     })
 })
@@ -14,21 +14,30 @@ fetch(url).then((response)=> {
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#weatherResult')
-weatherForm.addEventListener('submit',(e) => {
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault(); //stops form from reloading
     const location = search.value;
-    const url =  "/weather?city="+location;
-    fetch(url).then((response)=> {
-        response.json().then((data)=> {
-            if(data.error){
-                console.log(data.error)
-            }
-            else{
-            console.log(data);
-            
-            messageOne.textContent =  JSON.stringify(data);
-            }
-        })
+    const url = "/weather?city=" + location;
+    fetch(url).then((response) => {
+        if (response.json()) {
+            response.json().then((data) => {
+                if (data.error) {
+                    console.log(data.error)
+                }
+                else {
+                    console.log(data);
+
+                    messageOne.textContent = JSON.stringify(data);
+                }
+            })
+        }
+        else {
+            messageOne.textContent = response;
+            console.log(response);
+        }
+    }).catch((error) => {
+        messageOne.textContent = response;
+        console.log(error)
     })
-    
+
 })
